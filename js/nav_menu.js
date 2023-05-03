4903aea0f0e3118d0d6e2c94085d9a35a4f9ca3c
Jonatan Ruben <medinaarcos.jonatanruben@gmail.com>
2023-05-03 00:01:12 -0500

    Portfolio 3V 03/05/2023: Creacion git init

---
+++ js/nav_menu.js
@@ -0,0 +1,24 @@
+// Variables
+const btn = document.querySelector(".header__button");
+const nav = document.querySelector(".header__nav");
+
+// Menu hamburguesa
+btn.addEventListener("click", function(){
+    nav.classList.toggle('activo');
+})
+
+// Animacion
+window.addEventListener("scroll", function(){
+    const screen = document.documentElement.scrollTop;    
+    const header = document.querySelector(".header__section-nav");
+
+    if(screen >50){
+        header.classList.add('activo');
+    }
+    else{
+        header.classList.remove('activo');
+    }
+});
+
+
+
